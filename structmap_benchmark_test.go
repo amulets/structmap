238c9e81daec6778e64f589ad0edbdfb74f7ec7a
Otavio Tomasevic <supermock@hotmail.com.br>
2020-06-28 23:55:23 -0300

    add basic benchmark

---
+++ structmap_benchmark_test.go
@@ -0,0 +1,59 @@
+package structmap_test
+
+import (
+	"testing"
+
+	"github.com/amulets/structmap"
+	"github.com/amulets/structmap/behavior/cast"
+	"github.com/amulets/structmap/behavior/name"
+)
+
+type Benchmark struct {
+	String string
+	Number int
+	Slice  []string
+	Map    map[string]string
+}
+
+func BenchmarkDecode(b *testing.B) {
+	sm := structmap.New()
+	sm.AddBehavior(name.Noop)
+
+	from := map[string]interface{}{
+		"String": "MyString",
+		"Number": 1000,
+		"Slice":  []string{"1", "2", "3"},
+		"Map": map[string]string{
+			"A": "B",
+		},
+	}
+
+	var to Benchmark
+	for i := 0; i < b.N; i++ {
+		if err := sm.Decode(from, &to); err != nil {
+			b.Error(err)
+		}
+	}
+}
+
+func BenchmarkDecodeWithCast(b *testing.B) {
+	sm := structmap.New()
+	sm.AddBehavior(name.Noop)
+	sm.AddBehavior(cast.ToType)
+
+	from := map[string]interface{}{
+		"String": 2000,
+		"Number": "1000",
+		"Slice":  []int{1, 2, 3},
+		"Map": map[interface{}]interface{}{
+			"A": "B",
+		},
+	}
+
+	var to Benchmark
+	for i := 0; i < b.N; i++ {
+		if err := sm.Decode(from, &to); err != nil {
+			b.Error(err)
+		}
+	}
+}
